a5e997187885ceb0c05eb622d12b9b33d58b7b5d
JGW-Korea <jgw6372@gmail.com>
2023-06-13 19:40:25 +0900

    Redux 설정 완료

--- server/controllers/user/login/login.js
+++ server/controllers/user/login/login.js
@@ -13,21 +13,21 @@ router.post("/", async (req, res) => {
 
   // 요청된 이메일을 DB에서 찾는다.
   try {
-    const user = await User.findOne({
-      attributes: ["email", "password"],
+    const userInfo = await User.findOne({
       where: {
         email: { [Op.eq]: email },
       },
     });
 
-    if (!user) {
+    // DB에 값이 없을 경우
+    if (!userInfo) {
       return res.json({
         loginSuccess: false,
         message: "해당 이메일은 존재하지 않습니다.",
       });
     } else {
       // 요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
-      const isMatch = await bcrypt.compare(password, user.dataValues.password);
+      const isMatch = await bcrypt.compare(password, userInfo.password);
 
       if (!isMatch) {
         return res.json({
@@ -37,10 +37,10 @@ router.post("/", async (req, res) => {
       } else {
         // 이메일에 맞는 비밀번호라면 세션을 생성한다.
         req.session.save(() => {
-          req.session.userInfo = user;
-          res.status(200).json({
+          req.session.userId = userInfo.email;
+          res.json({
             loginSuccess: true,
-            data: req.session.userInfo.email,
+            data: userInfo.email,
           });
         });
       }
--- server/controllers/user/logout/logout.js
+++ server/controllers/user/logout/logout.js
@@ -3,10 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  // logout을 하기위해 session 값을 삭제한다.
-  req.session.destroy(() => {
-    res.status(200).json({ message: "logout success" });
-  });
+  // 로그인이 되어있지 않을경우에 로그아웃에 접근할 경우
+  if (!req.session.userId) {
+    res.status(400).json({
+      logoutSuccess: false,
+      message: "logout faild",
+    });
+  } else {
+    // logout을 하기위해 session 값을 삭제한다.
+    req.session.destroy(() => {
+      res.status(200).json({ message: "logout success" });
+    });
+  }
 });
 
 module.exports = router;
